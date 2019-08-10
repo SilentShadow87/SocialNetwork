88cc9f3a18b727cdaa66ac31afcb82961aab8dea
vladimir <vlada@way2cu.com>
2019-08-10 19:56:27 +0200

    Add like and unlike funcionality

--- test_site/api/urls.py
+++ test_site/api/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
 	path('user/create', views.UserCreateAPIView.as_view(), name='register'),
 	path('post/list', views.PostListAPIView.as_view(), name='post_list'),
 	path('post/create', views.PostCreateAPIView.as_view(), name='post_create'),
-	re_path('post/(?P<pk>\d+)', views.PostDetailAPIView.as_view(), name='post_detail')
+	re_path('^post/(?P<pk>\d+/$)', views.PostDetailAPIView.as_view(), name='post_detail'),
+	re_path('^post/like/(?P<pk>\d+)/$', views.PostLikeAPIView.as_view(), name='post_like'),
+	re_path('^post/unlike/(?P<pk>\d+)/$', views.PostUnlikeAPIView.as_view(), name='post_unlike')
 ]
--- test_site/api/views.py
+++ test_site/api/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import get_user_model
 
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import UserSerializer, PostListSerializer, PostCreateSerializer
 from .models import Post
@@ -38,3 +41,26 @@ class PostCreateAPIView(CreateAPIView):
 
 	def perform_create(self, serializer):
 		serializer.save(author=self.request.user)
+
+
+class PostLikeAPIView(APIView):
+	def get(self, request, pk):
+		post = get_object_or_404(Post, pk=pk)
+		user = self.request.user
+
+		if user in post.likes.all():
+			message = {'message': 'User already liked this post.'}
+			return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+		post.likes.add(user)
+		return Response(status=status.HTTP_200_OK)
+
+
+class PostUnlikeAPIView(APIView):
+	def get(self, request, pk):
+		post = get_object_or_404(Post, pk=pk)
+		user = self.request.user
+
+		post.likes.remove(user)
+		return Response(status=status.HTTP_200_OK)
+
